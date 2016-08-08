fdb2aa15f133fabd9d285d04c14555c72202be0f
Uzair Sajid <uzee@uzeeinc.net>
2016-08-08 17:44:19 +0500

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,20 @@
 
 let gulp = require('gulp');
 let lec = require('gulp-line-ending-corrector');
+let ts = require('gulp-typescript');
+let sourcemaps = require('gulp-sourcemaps');
 
-// correct line-endings
-gulp.task('line-endings', () => {
+// compile and build the project
+gulp.task('default', () => {
 
-  return gulp.src('bin/**/*.js')
+  let tsProject = ts.createProject('tsconfig.json');
+
+  let tsResult = tsProject.src('src/**/*.ts')
+    .pipe(sourcemaps.init())
+    .pipe(ts(tsProject, undefined, ts.reporter.defaultReporter()));
+
+  return tsResult.js
+    .pipe(sourcemaps.write('./'))
     .pipe(lec())
     .pipe(gulp.dest('bin'));
 });
